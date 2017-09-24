337c308dd53fbbba7e16485f7647611d69b54724
skumarlabs <sur365@gmail.com>
2017-09-25 00:02:27 +0530

    added conll2000 dataset

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     text2 = fa.read_text(filename=datadir+'/enwiki-20170620-pages-articles.xml-2')
     print('reading input 2 done')
     fa.create_word2idx(text1, text2)
-    #print_matrix(word_idx, dict_len, num_docs)
+    fa.print_matrix(fa.word_idx, fa.dict_len, fa.num_docs)
     #findClosest(getDistance('cat', 'dog', 'lion'))
     print('to you')
 
